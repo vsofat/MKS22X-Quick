5c84c6e0d62b62803c15fa559ad4dcea6fc866fc
vsofat <vsofat00@stuy.edu>
2019-03-18 10:11:52 -0400

    fixing errors in partition whihc dont let sort work

--- Quick.java
+++ Quick.java
@@ -3,18 +3,50 @@
 public class Quick{
 
   public static void main(String[] args) {
-  int[] ary = {1000, 999,999,999,0,0,1000,1000,0};;
+  /*int[] ary = {1000, 999,999,999,0,0,1000,1000,0};;
   //System.out.println("ANSWER(999): " + quickselect(ary, 10));
   partition(ary,0,ary.length);
   System.out.println(toString(ary));
-  //System.out.println(quickselect(ary,3));
+  //System.out.println(quickselect(ary,3));*/
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Quick.quicksort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
+  }
 }
 
   public static int partition (int [] data, int start, int end) {
 
-  int high = start;
-  int holder = end + 1;
-  int low = end;
+  int low = start;
+  int holder = start + 1;
+  int high = end;
 
   if (low == high){
     return low;
@@ -29,22 +61,21 @@ public class Quick{
   //debugging
   //System.out.println("Pivot is currently: " + data[0]);
 
-  while (holder <= start) {
-    if (data[holder] > pivot) {
-      swapValues(data, holder, low);
-      end --;
+  while (holder <= high) {
+    if (data[holder] < pivot) {
+      holder++;
     }
-    else if (data[holder] < pivot){
-      swapValues(data, holder, high);
-      start ++;
-      holder ++;
+    else if (data[holder] == pivot && pivotIndex <= high){
+      swapValues(data, holder, pivotIndex);
+      pivotIndex++;
     }
     else{
-      holder++;
+      swapValues(data, high, holder);
+      high++;
     }
   }
 
-  swapValues(data, high, low);
+  swapValues(data, low, high);
 
   return high;
 }
